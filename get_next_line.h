b7af631a8c661048d51520f81d7e1632f64232d4
lea medrano <leamedrano@gmail.com>
2022-11-14 11:38:27 +0100

    adding buffer size + guidelines to gnl understanding

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: lmedrano <lmedrano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 11:06:28 by lmedrano          #+#    #+#             */
-/*   Updated: 2022/11/14 11:09:37 by lmedrano         ###   ########.fr       */
+/*   Updated: 2022/11/14 11:29:02 by lmedrano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
+#define BUFF_SIZE 42
 
 char	*get_next_line(int fd);
 
